fb48ad7a60340f85bcdf5a023e0d21f1c3704a8e
Max Radin <radin.max@gmail.com>
2020-07-21 12:30:46 -0400

    Refactoring psi4 test

--- src/python/qepsi4/_psi4_test.py
+++ src/python/qepsi4/_psi4_test.py
@@ -1,35 +1,35 @@
-import unittest
 from qepsi4 import run_psi4
 from openfermion import (
     jordan_wigner,
     jw_get_ground_state_at_particle_number,
     qubit_operator_sparse,
 )
+import numpy as np
+import math
 
 
-class TestChem(unittest.TestCase):
-    def test_run_psi4(self):
+def test_run_psi4(self):
 
-        geometry = {
-            "sites": [
-                {"species": "H", "x": 0, "y": 0, "z": 0},
-                {"species": "H", "x": 0, "y": 0, "z": 1.7},
-            ]
-        }
+    geometry = {
+        "sites": [
+            {"species": "H", "x": 0, "y": 0, "z": 0},
+            {"species": "H", "x": 0, "y": 0, "z": 1.7},
+        ]
+    }
 
-        results, hamiltonian = run_psi4(geometry, save_hamiltonian=True)
-        self.assertAlmostEqual(results["energy"], -0.8544322638069642)
-        self.assertEqual(results["n_alpha"], 1)
-        self.assertEqual(results["n_beta"], 1)
-        self.assertEqual(results["n_mo"], 2)
-        self.assertEqual(results["n_frozen_core"], 0)
-        self.assertEqual(results["n_frozen_valence"], 0)
+    results, hamiltonian = run_psi4(geometry, save_hamiltonian=True)
+    assert math.isclose(results["energy"], -0.8544322638069642)
+    assert results["n_alpha"] == 1
+    assert results["n_beta"] == 1
+    assert results["n_mo"] == 2
+    assert results["n_frozen_core"] == 0
+    assert results["n_frozen_valence"] == 0
 
-        self.assertEqual(hamiltonian.n_qubits, 4)
-        qubit_operator = qubit_operator_sparse(jordan_wigner(hamiltonian))
-        energy, state = jw_get_ground_state_at_particle_number(qubit_operator, 2)
+    assert hamiltonian.n_qubits == 4
+    qubit_operator = qubit_operator_sparse(jordan_wigner(hamiltonian))
+    energy, state = jw_get_ground_state_at_particle_number(qubit_operator, 2)
 
-        results_cisd, hamiltonian = run_psi4(geometry, method="ccsd")
+    results_cisd, hamiltonian = run_psi4(geometry, method="ccsd")
 
-        # For this system, the CCSD energy should be exact.
-        self.assertAlmostEqual(energy, results_cisd["energy"])
+    # For this system, the CCSD energy should be exact.
+    assert math.isclose(energy, results_cisd["energy"])
