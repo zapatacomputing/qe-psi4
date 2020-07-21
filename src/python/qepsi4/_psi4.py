16b5c5e25a72135f6a040c27c073785ece70ee03
Max Radin <radin.max@gmail.com>
2020-07-21 11:17:27 -0400

    Formatting with black

--- src/python/qepsi4/_psi4.py
+++ src/python/qepsi4/_psi4.py
@@ -3,11 +3,21 @@ import numpy as np
 from openfermion import InteractionOperator, general_basis_change, MolecularData
 from openfermion.config import EQ_TOLERANCE
 
-def run_psi4(geometry, basis='STO-3G', multiplicity=1, charge=0,
-        method='scf', reference='rhf',
-        freeze_core=False, n_active=None,
-        save_hamiltonian=False, options=None,
-        n_active_extract=None, freeze_core_extract=False):
+
+def run_psi4(
+    geometry,
+    basis="STO-3G",
+    multiplicity=1,
+    charge=0,
+    method="scf",
+    reference="rhf",
+    freeze_core=False,
+    n_active=None,
+    save_hamiltonian=False,
+    options=None,
+    n_active_extract=None,
+    freeze_core_extract=False,
+):
     """Generate an input file in the Psi4 python domain-specific language for
     a molecule.
     
@@ -34,29 +44,28 @@ def run_psi4(geometry, basis='STO-3G', multiplicity=1, charge=0,
             (openfermion.InteractionOperator).
     """
 
-    geometry_str = f'{charge} {multiplicity}\n'
-    for atom in geometry['sites']:
-        geometry_str += '{} {} {} {}\n'.format(atom['species'],
-                                               atom['x'],
-                                               atom['y'],
-                                               atom['z'])
+    geometry_str = f"{charge} {multiplicity}\n"
+    for atom in geometry["sites"]:
+        geometry_str += "{} {} {} {}\n".format(
+            atom["species"], atom["x"], atom["y"], atom["z"]
+        )
 
-    geometry_str += '\nunits angstrom\n'
+    geometry_str += "\nunits angstrom\n"
     c1_sym = save_hamiltonian or n_active
     if c1_sym:
-        geometry_str += 'symmetry c1\n'
-    
+        geometry_str += "symmetry c1\n"
+
     molecule = psi4.geometry(geometry_str)
-    psi4.set_options({'reference': reference, 'basis': basis})
+    psi4.set_options({"reference": reference, "basis": basis})
     energy, wavefunction = psi4.energy(method, return_wfn=True)
 
     results = {
-        'energy': energy,
-        'n_alpha': wavefunction.nalpha(),
-        'n_beta': wavefunction.nbeta(),
-        'n_mo': wavefunction.nmo(),
-        'n_frozen_core': wavefunction.nfrzc(),
-        'n_frozen_valence': wavefunction.frzvpi().sum()
+        "energy": energy,
+        "n_alpha": wavefunction.nalpha(),
+        "n_beta": wavefunction.nbeta(),
+        "n_mo": wavefunction.nmo(),
+        "n_frozen_core": wavefunction.nfrzc(),
+        "n_frozen_valence": wavefunction.frzvpi().sum(),
     }
 
     hamiltonian = None
@@ -70,17 +79,26 @@ def run_psi4(geometry, basis='STO-3G', multiplicity=1, charge=0,
                 n_orbitals += wavefunction.nfrzc()
             orbitals = orbitals[:, :n_orbitals]
             orbitals = psi4.core.Matrix.from_array(orbitals)
-        hamiltonian = get_ham_from_psi4(wavefunction,
-                                        mints,
-                                        n_active_extract,
-                                        freeze_core_extract,
-                                        orbitals,
-                                        molecule.nuclear_repulsion_energy())
+        hamiltonian = get_ham_from_psi4(
+            wavefunction,
+            mints,
+            n_active_extract,
+            freeze_core_extract,
+            orbitals,
+            molecule.nuclear_repulsion_energy(),
+        )
 
     return results, hamiltonian
 
-def get_ham_from_psi4(wfn, mints, n_active_extract=None, freeze_core_extract=False, 
-                      orbs=None, nuclear_repulsion_energy=0):
+
+def get_ham_from_psi4(
+    wfn,
+    mints,
+    n_active_extract=None,
+    freeze_core_extract=False,
+    orbs=None,
+    nuclear_repulsion_energy=0,
+):
     """Get a molecular Hamiltonian from a Psi4 calculation.
 
     Args:
@@ -101,8 +119,10 @@ def get_ham_from_psi4(wfn, mints, n_active_extract=None, freeze_core_extract=Fal
             included.
     """
 
-    assert wfn.same_a_b_orbs(), "Extraction of Hamiltonian from wavefunction" + \
-        "with different alpha and beta orbitals not yet supported :("
+    assert wfn.same_a_b_orbs(), (
+        "Extraction of Hamiltonian from wavefunction"
+        + "with different alpha and beta orbitals not yet supported :("
+    )
 
     # Note: code refactored to use Psi4 integral-transformation routines
     # no more storing the whole two-electron integral tensor when only an
@@ -110,9 +130,11 @@ def get_ham_from_psi4(wfn, mints, n_active_extract=None, freeze_core_extract=Fal
 
     orbitals = wfn.Ca().to_array(dense=True)
     one_body_integrals = general_basis_change(
-        np.asarray(mints.ao_kinetic()), orbitals, (1, 0))
+        np.asarray(mints.ao_kinetic()), orbitals, (1, 0)
+    )
     one_body_integrals += general_basis_change(
-        np.asarray(mints.ao_potential()), orbitals, (1, 0))
+        np.asarray(mints.ao_potential()), orbitals, (1, 0)
+    )
 
     # Build the transformation matrices, i.e. the orbitals for which
     # we want the integrals, as Psi4.core.Matrix objects
@@ -123,29 +145,30 @@ def get_ham_from_psi4(wfn, mints, n_active_extract=None, freeze_core_extract=Fal
         trf_mat = wfn.Ca()
         n_active_extract = wfn.nmo() - n_core_extract
     else:
-    # If orbs is given, it allows us to perform the two-electron integrals
-    # transformation only in the space of active orbitals. Otherwise, we
-    # transform all orbitals and filter them out in the get_ham_from_integrals
-    # function
+        # If orbs is given, it allows us to perform the two-electron integrals
+        # transformation only in the space of active orbitals. Otherwise, we
+        # transform all orbitals and filter them out in the get_ham_from_integrals
+        # function
         if orbs is None:
             trf_mat = wfn.Ca()
         else:
-            assert(orbs.to_array(dense=True).shape[1] == n_active_extract + n_core_extract)
+            assert (
+                orbs.to_array(dense=True).shape[1] == n_active_extract + n_core_extract
+            )
             trf_mat = orbs
 
     two_body_integrals = np.asarray(mints.mo_eri(trf_mat, trf_mat, trf_mat, trf_mat))
     n_orbitals = trf_mat.shape[1]
-    two_body_integrals.reshape((n_orbitals, n_orbitals,
-                                n_orbitals, n_orbitals))
-    two_body_integrals = np.einsum('psqr', two_body_integrals)
+    two_body_integrals.reshape((n_orbitals, n_orbitals, n_orbitals, n_orbitals))
+    two_body_integrals = np.einsum("psqr", two_body_integrals)
 
     # Truncate
-    one_body_integrals[np.absolute(one_body_integrals) < EQ_TOLERANCE] = 0.
-    two_body_integrals[np.absolute(two_body_integrals) < EQ_TOLERANCE] = 0.
+    one_body_integrals[np.absolute(one_body_integrals) < EQ_TOLERANCE] = 0.0
+    two_body_integrals[np.absolute(two_body_integrals) < EQ_TOLERANCE] = 0.0
 
     if n_active_extract is None and not freeze_core_extract:
         occupied_indices = None
-        active_indices = None    
+        active_indices = None
     else:
         # Indices of occupied molecular orbitals
         occupied_indices = range(n_core_extract)
@@ -154,14 +177,15 @@ def get_ham_from_psi4(wfn, mints, n_active_extract=None, freeze_core_extract=Fal
         active_indices = range(n_core_extract, n_core_extract + n_active_extract)
 
     # In order to keep the MolecularData class happy, we need a 'valid' molecule
-    molecular_data = MolecularData(geometry=[('H', (0, 0, 0))],
-                                   basis='',
-                                   multiplicity=2)
+    molecular_data = MolecularData(
+        geometry=[("H", (0, 0, 0))], basis="", multiplicity=2
+    )
 
     molecular_data.one_body_integrals = one_body_integrals
     molecular_data.two_body_integrals = two_body_integrals
     molecular_data.nuclear_repulsion = nuclear_repulsion_energy
-    hamiltonian = molecular_data.get_molecular_hamiltonian(occupied_indices,
-                                            active_indices)
+    hamiltonian = molecular_data.get_molecular_hamiltonian(
+        occupied_indices, active_indices
+    )
 
-    return(hamiltonian)
+    return hamiltonian
--- src/python/qepsi4/_psi4_test.py
+++ src/python/qepsi4/_psi4_test.py
@@ -1,30 +1,35 @@
 import unittest
 from qepsi4 import run_psi4
-from openfermion import (jordan_wigner, jw_get_ground_state_at_particle_number,
-                         qubit_operator_sparse)
+from openfermion import (
+    jordan_wigner,
+    jw_get_ground_state_at_particle_number,
+    qubit_operator_sparse,
+)
+
 
 class TestChem(unittest.TestCase):
-
     def test_run_psi4(self):
 
-        geometry = {"sites": [
-            {'species': 'H', 'x': 0, 'y': 0, 'z': 0},
-            {'species': 'H', 'x': 0, 'y': 0, 'z': 1.7}
-        ]}
+        geometry = {
+            "sites": [
+                {"species": "H", "x": 0, "y": 0, "z": 0},
+                {"species": "H", "x": 0, "y": 0, "z": 1.7},
+            ]
+        }
 
         results, hamiltonian = run_psi4(geometry, save_hamiltonian=True)
-        self.assertAlmostEqual(results['energy'], -0.8544322638069642)
-        self.assertEqual(results['n_alpha'], 1)
-        self.assertEqual(results['n_beta'], 1)
-        self.assertEqual(results['n_mo'], 2)
-        self.assertEqual(results['n_frozen_core'], 0)
-        self.assertEqual(results['n_frozen_valence'], 0)
+        self.assertAlmostEqual(results["energy"], -0.8544322638069642)
+        self.assertEqual(results["n_alpha"], 1)
+        self.assertEqual(results["n_beta"], 1)
+        self.assertEqual(results["n_mo"], 2)
+        self.assertEqual(results["n_frozen_core"], 0)
+        self.assertEqual(results["n_frozen_valence"], 0)
 
         self.assertEqual(hamiltonian.n_qubits, 4)
         qubit_operator = qubit_operator_sparse(jordan_wigner(hamiltonian))
         energy, state = jw_get_ground_state_at_particle_number(qubit_operator, 2)
-        
-        results_cisd, hamiltonian = run_psi4(geometry, method='ccsd')
+
+        results_cisd, hamiltonian = run_psi4(geometry, method="ccsd")
 
         # For this system, the CCSD energy should be exact.
-        self.assertAlmostEqual(energy, results_cisd['energy'])
+        self.assertAlmostEqual(energy, results_cisd["energy"])
