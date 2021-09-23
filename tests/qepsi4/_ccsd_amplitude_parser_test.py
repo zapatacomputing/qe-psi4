fbfa04d516dc37941b3e4cc4f92e676d96f3835f
Shiro-Raven <exclass9.24@gmail.com>
2021-09-23 16:06:47 +0200

    ZQS-767 refactor tests using parameterization

--- tests/qepsi4/_ccsd_amplitude_parser_test.py
+++ tests/qepsi4/_ccsd_amplitude_parser_test.py
@@ -1,8 +1,9 @@
+import pytest
 from qepsi4 import run_psi4, parse_amplitudes_from_psi4_ccsd
 from openfermion import FermionOperator
 
 
-def get_expected_fop():
+def get_expected_mp2_fop():
     result_fop = FermionOperator()
     result_fop += -0.0193104873 * FermionOperator("4^ 0 5^ 1")
     result_fop += -0.0193104873 * FermionOperator("5^ 1 4^ 0")
@@ -18,59 +19,7 @@ def get_expected_fop():
     return result_fop
 
 
-### MP2 amplitude extraction test ###
-
-
-def test_h2_mp2_amplitudes_parsed_correctly():
-    basis = "6-31g"
-
-    options = {"mp2_amps_print": True, "num_amps_print": 100}
-
-    output_filename = "h2_sto3g"
-
-    hydrogen_geometry = {
-        "sites": [
-            {"species": "H", "x": 0, "y": 0, "z": 0},
-            {"species": "H", "x": 0, "y": 0, "z": 0.2},
-        ]
-    }
-
-    out = run_psi4(
-        geometry=hydrogen_geometry,
-        basis=basis,
-        multiplicity=1,
-        charge=0,
-        method="ccsd",
-        reference="rhf",
-        freeze_core=False,
-        save_hamiltonian=True,
-        options=options,
-        n_active_extract=None,
-        n_occupied_extract=None,
-        freeze_core_extract=False,
-        save_rdms=False,
-        output_filename=output_filename,
-        return_wfn=True,
-    )
-
-    _, wfn = out
-
-    given_fop = parse_amplitudes_from_psi4_ccsd(
-        wfn=wfn,
-        psi_filename="{0}.dat".format(output_filename),
-        n_active_extract=None,
-        freeze_core_extract=False,
-        freeze_core=False,
-        n_frozen_amplitudes=0,
-        get_mp2_amplitudes=True,
-    )
-
-    expected_fop = get_expected_fop()
-
-    assert expected_fop == given_fop
-
-
-def get_expected_ccsd_fop():
+def get_expected_h2_ccsd_fop():
     result_fop = FermionOperator()
     result_fop += -0.0252893416 * FermionOperator("4^ 0 5^ 1")
     result_fop += -0.0252893416 * FermionOperator("5^ 1 4^ 0")
@@ -88,63 +37,7 @@ def get_expected_ccsd_fop():
     return result_fop
 
 
-### CCSD amplitude extraction test ###
-
-
-def test_h2_ccsd_amplitudes_parsed_correctly():
-    basis = "6-31g"
-
-    options = {"mp2_amps_print": True, "num_amps_print": 100}
-
-    output_filename = "h2_sto3g"
-
-    hydrogen_geometry = {
-        "sites": [
-            {"species": "H", "x": 0, "y": 0, "z": 0},
-            {"species": "H", "x": 0, "y": 0, "z": 0.2},
-        ]
-    }
-
-    out = run_psi4(
-        geometry=hydrogen_geometry,
-        basis=basis,
-        multiplicity=1,
-        charge=0,
-        method="ccsd",
-        reference="rhf",
-        freeze_core=False,
-        save_hamiltonian=True,
-        options=options,
-        n_active_extract=None,
-        n_occupied_extract=None,
-        freeze_core_extract=False,
-        save_rdms=False,
-        output_filename=output_filename,
-        return_wfn=True,
-    )
-
-    _, wfn = out
-
-    given_fop = parse_amplitudes_from_psi4_ccsd(
-        wfn=wfn,
-        psi_filename="{0}.dat".format(output_filename),
-        n_active_extract=None,
-        freeze_core_extract=False,
-        freeze_core=False,
-        n_frozen_amplitudes=0,
-        get_mp2_amplitudes=False,
-    )
-
-    expected_fop = get_expected_ccsd_fop()
-
-    assert expected_fop == given_fop
-
-
-### CCSD amplitude extraction test with n_active_extract ###
-
-
-def test_h2_ccsd_amplitudes_parsed_correctly_with_active_extract():
-
+def get_expected_h2_ccsd_fop_n_active_extract():
     ref_fop = FermionOperator()
     ref_fop += -0.0252893416 * FermionOperator("4^ 0 5^ 1")
     ref_fop += -0.0252893416 * FermionOperator("5^ 1 4^ 0")
@@ -153,60 +46,10 @@ def test_h2_ccsd_amplitudes_parsed_correctly_with_active_extract():
     ref_fop += -0.0047660845 * FermionOperator("4^ 0")
     ref_fop += -0.0047660845 * FermionOperator("5^ 1")
 
-    basis = "6-31g"
-
-    options = {"mp2_amps_print": True, "num_amps_print": 100}
-
-    output_filename = "h2_sto3g"
-
-    hydrogen_geometry = {
-        "sites": [
-            {"species": "H", "x": 0, "y": 0, "z": 0},
-            {"species": "H", "x": 0, "y": 0, "z": 0.2},
-        ]
-    }
-
-    out = run_psi4(
-        geometry=hydrogen_geometry,
-        basis=basis,
-        multiplicity=1,
-        charge=0,
-        method="ccsd",
-        reference="rhf",
-        freeze_core=False,
-        save_hamiltonian=True,
-        options=options,
-        n_active_extract=None,
-        n_occupied_extract=None,
-        freeze_core_extract=False,
-        save_rdms=False,
-        output_filename=output_filename,
-        return_wfn=True,
-    )
-
-    _, wfn = out
-
-    given_fop = parse_amplitudes_from_psi4_ccsd(
-        wfn=wfn,
-        psi_filename="{0}.dat".format(output_filename),
-        n_active_extract=3,
-        freeze_core_extract=False,
-        freeze_core=False,
-        n_frozen_amplitudes=0,
-        get_mp2_amplitudes=False,
-    )
-
-    expected_fop = ref_fop
-    print("Given")
-    print(given_fop)
-    print("Expected")
-    print(expected_fop)
-
-    assert expected_fop == given_fop
+    return ref_fop
 
 
-### CCSD amplitude extraction test with freeze_core ###
-def get_expected_ccsd_fop_frozen_core_LiH():
+def get_expected_LiH_ccsd_fop_frozen_core():
     result_fop = FermionOperator()
     result_fop += -0.1071079862 * FermionOperator("2^ 0 3^ 1")
     result_fop += -0.1071079862 * FermionOperator("3^ 1 2^ 0")
@@ -228,120 +71,7 @@ def get_expected_ccsd_fop_frozen_core_LiH():
     return result_fop
 
 
-def test_frozen_core_lih_ccsd_amplitudes_parsed_correctly():
-    basis = "sto-3g"
-
-    options = {"mp2_amps_print": True, "num_amps_print": 100}
-
-    output_filename = "lih_sto3g"
-
-    LiH_geometry = {
-        "sites": [
-            {"species": "Li", "x": 0, "y": 0, "z": 0},
-            {"species": "H", "x": 0, "y": 0, "z": 0.2},
-        ]
-    }
-
-    out = run_psi4(
-        geometry=LiH_geometry,
-        basis=basis,
-        multiplicity=1,
-        charge=0,
-        method="ccsd",
-        reference="rhf",
-        freeze_core=True,
-        save_hamiltonian=True,
-        options=options,
-        n_active_extract=None,
-        n_occupied_extract=None,
-        freeze_core_extract=False,
-        save_rdms=False,
-        output_filename=output_filename,
-        return_wfn=True,
-    )
-
-    _, wfn = out
-
-    given_fop = parse_amplitudes_from_psi4_ccsd(
-        wfn=wfn,
-        psi_filename="{0}.dat".format(output_filename),
-        n_active_extract=None,
-        freeze_core_extract=False,
-        freeze_core=True,
-        n_frozen_amplitudes=0,
-        get_mp2_amplitudes=False,
-    )
-
-    expected_fop = get_expected_ccsd_fop_frozen_core_LiH()
-
-    assert expected_fop == given_fop
-
-
-### CCSD amplitude extraction test with freeze_core and n_active_extract ###
-
-
-def test_frozen_core_lih_ccsd_amplitudes_parsed_correctly_with_n_active():
-
-    ref_fop = FermionOperator()
-    ref_fop += -0.1071079862 * FermionOperator("2^ 0 3^ 1")
-    ref_fop += -0.1071079862 * FermionOperator("3^ 1 2^ 0")
-    ref_fop += -0.10207494545 * FermionOperator("4^ 0 5^ 1")
-    ref_fop += -0.10207494545 * FermionOperator("5^ 1 4^ 0")
-    ref_fop += -0.0603059476 * FermionOperator("2^ 0")
-    ref_fop += -0.0603059476 * FermionOperator("3^ 1")
-
-    basis = "sto-3g"
-
-    options = {"mp2_amps_print": True, "num_amps_print": 100}
-
-    output_filename = "lih_sto3g"
-
-    LiH_geometry = {
-        "sites": [
-            {"species": "Li", "x": 0, "y": 0, "z": 0},
-            {"species": "H", "x": 0, "y": 0, "z": 0.2},
-        ]
-    }
-
-    out = run_psi4(
-        geometry=LiH_geometry,
-        basis=basis,
-        multiplicity=1,
-        charge=0,
-        method="ccsd",
-        reference="rhf",
-        freeze_core=True,
-        save_hamiltonian=True,
-        options=options,
-        n_active_extract=None,
-        n_occupied_extract=None,
-        freeze_core_extract=False,
-        save_rdms=False,
-        output_filename=output_filename,
-        return_wfn=True,
-    )
-
-    _, wfn = out
-
-    given_fop = parse_amplitudes_from_psi4_ccsd(
-        wfn=wfn,
-        psi_filename="{0}.dat".format(output_filename),
-        n_active_extract=3,
-        freeze_core_extract=False,
-        freeze_core=True,
-        n_frozen_amplitudes=0,
-        get_mp2_amplitudes=False,
-    )
-
-    expected_fop = ref_fop
-
-    assert expected_fop == given_fop
-
-
-### CCSD amplitude extraction test with freeze_core_extract ###
-
-
-def get_expected_ccsd_fop_freeze_core_extract_LiH():
+def get_expected_LiH_ccsd_fop_freeze_core_extract():
     result_fop = FermionOperator()
     result_fop += -0.1068653324 * FermionOperator("2^ 0 3^ 1")
     result_fop += -0.1068653324 * FermionOperator("3^ 1 2^ 0")
@@ -363,60 +93,19 @@ def get_expected_ccsd_fop_freeze_core_extract_LiH():
     return result_fop
 
 
-def test_freeze_core_extract_lih_ccsd_amplitudes_parsed_correctly():
-    basis = "sto-3g"
+def get_expected_LiH_ccsd_fop_active_extract_frozen_core():
+    ref_fop = FermionOperator()
+    ref_fop += -0.1071079862 * FermionOperator("2^ 0 3^ 1")
+    ref_fop += -0.1071079862 * FermionOperator("3^ 1 2^ 0")
+    ref_fop += -0.10207494545 * FermionOperator("4^ 0 5^ 1")
+    ref_fop += -0.10207494545 * FermionOperator("5^ 1 4^ 0")
+    ref_fop += -0.0603059476 * FermionOperator("2^ 0")
+    ref_fop += -0.0603059476 * FermionOperator("3^ 1")
 
-    options = {"mp2_amps_print": True, "num_amps_print": 100}
-
-    output_filename = "lih_sto3g"
-
-    LiH_geometry = {
-        "sites": [
-            {"species": "Li", "x": 0, "y": 0, "z": 0},
-            {"species": "H", "x": 0, "y": 0, "z": 0.2},
-        ]
-    }
-
-    out = run_psi4(
-        geometry=LiH_geometry,
-        basis=basis,
-        multiplicity=1,
-        charge=0,
-        method="ccsd",
-        reference="rhf",
-        freeze_core=False,
-        save_hamiltonian=True,
-        options=options,
-        n_active_extract=None,
-        n_occupied_extract=None,
-        freeze_core_extract=False,
-        save_rdms=False,
-        output_filename=output_filename,
-        return_wfn=True,
-    )
-
-    _, wfn = out
-
-    given_fop = parse_amplitudes_from_psi4_ccsd(
-        wfn=wfn,
-        psi_filename="{0}.dat".format(output_filename),
-        n_active_extract=None,
-        freeze_core_extract=True,
-        freeze_core=False,
-        n_frozen_amplitudes=1,
-        get_mp2_amplitudes=False,
-    )
-
-    expected_fop = get_expected_ccsd_fop_freeze_core_extract_LiH()
-
-    assert expected_fop == given_fop
+    return ref_fop
 
 
-### CCSD amplitude extraction test with freeze_core_extract and n_active_extract ###
-
-
-def test_freeze_core_extract_lih_ccsd_amplitudes_parsed_correctly_with_n_active():
-
+def get_expected_ccsd_fop_freeze_core_extract_n_active_extract():
     ref_fop = FermionOperator()
     ref_fop += -0.1068653324 * FermionOperator("2^ 0 3^ 1")
     ref_fop += -0.1068653324 * FermionOperator("3^ 1 2^ 0")
@@ -425,27 +114,155 @@ def test_freeze_core_extract_lih_ccsd_amplitudes_parsed_correctly_with_n_active(
     ref_fop += -0.0618151355 * FermionOperator("2^ 0")
     ref_fop += -0.0618151355 * FermionOperator("3^ 1")
 
-    basis = "sto-3g"
+    return ref_fop
 
-    options = {"mp2_amps_print": True, "num_amps_print": 100}
 
-    output_filename = "lih_sto3g"
+options = {"mp2_amps_print": True, "num_amps_print": 100}
 
-    LiH_geometry = {
-        "sites": [
-            {"species": "Li", "x": 0, "y": 0, "z": 0},
-            {"species": "H", "x": 0, "y": 0, "z": 0.2},
-        ]
-    }
 
+@pytest.mark.parametrize(
+    "expected_fop_fn,basis,output_filename,geometry,\
+        n_active_extract,freeze_core_extract,freeze_core,n_frozen_amplitudes,get_mp2_amplitudes",
+    [  # MP2 amplitude extraction test
+        (
+            get_expected_mp2_fop,
+            "6-31g",
+            "h2_sto3g",
+            {
+                "sites": [
+                    {"species": "H", "x": 0, "y": 0, "z": 0},
+                    {"species": "H", "x": 0, "y": 0, "z": 0.2},
+                ]
+            },
+            None,
+            False,
+            False,
+            0,
+            True,
+        ),
+        # CCSD amplitude extraction test
+        (
+            get_expected_h2_ccsd_fop,
+            "6-31g",
+            "h2_sto3g",
+            {
+                "sites": [
+                    {"species": "H", "x": 0, "y": 0, "z": 0},
+                    {"species": "H", "x": 0, "y": 0, "z": 0.2},
+                ]
+            },
+            None,
+            False,
+            False,
+            0,
+            False,
+        ),
+        # CCSD amplitude extraction test with n_active_extract
+        (
+            get_expected_h2_ccsd_fop_n_active_extract,
+            "6-31g",
+            "h2_sto3g",
+            {
+                "sites": [
+                    {"species": "H", "x": 0, "y": 0, "z": 0},
+                    {"species": "H", "x": 0, "y": 0, "z": 0.2},
+                ]
+            },
+            3,
+            False,
+            False,
+            0,
+            False,
+        ),
+        # CCSD amplitude extraction test with freeze_core
+        (
+            get_expected_LiH_ccsd_fop_frozen_core,
+            "sto-3g",
+            "lih_sto3g",
+            {
+                "sites": [
+                    {"species": "Li", "x": 0, "y": 0, "z": 0},
+                    {"species": "H", "x": 0, "y": 0, "z": 0.2},
+                ]
+            },
+            None,
+            False,
+            True,
+            0,
+            False,
+        ),
+        # CCSD amplitude extraction test with freeze_core and n_active_extract
+        (
+            get_expected_LiH_ccsd_fop_active_extract_frozen_core,
+            "sto-3g",
+            "lih_sto3g",
+            {
+                "sites": [
+                    {"species": "Li", "x": 0, "y": 0, "z": 0},
+                    {"species": "H", "x": 0, "y": 0, "z": 0.2},
+                ]
+            },
+            3,
+            False,
+            True,
+            0,
+            False,
+        ),
+        # CCSD amplitude extraction test with freeze_core_extract
+        (
+            get_expected_LiH_ccsd_fop_freeze_core_extract,
+            "sto-3g",
+            "lih_sto3g",
+            {
+                "sites": [
+                    {"species": "Li", "x": 0, "y": 0, "z": 0},
+                    {"species": "H", "x": 0, "y": 0, "z": 0.2},
+                ]
+            },
+            None,
+            True,
+            False,
+            1,
+            False,
+        ),
+        # CCSD amplitude extraction test with freeze_core_extract and n_active_extract
+        (
+            get_expected_ccsd_fop_freeze_core_extract_n_active_extract,
+            "sto-3g",
+            "lih_sto3g",
+            {
+                "sites": [
+                    {"species": "Li", "x": 0, "y": 0, "z": 0},
+                    {"species": "H", "x": 0, "y": 0, "z": 0.2},
+                ]
+            },
+            3,
+            True,
+            False,
+            1,
+            False,
+        ),
+    ],
+)
+def test_parse_amplitudes_from_psi4_ccsd(
+    expected_fop_fn,
+    basis,
+    output_filename,
+    geometry,
+    n_active_extract,
+    freeze_core_extract,
+    freeze_core,
+    n_frozen_amplitudes,
+    get_mp2_amplitudes,
+):
     out = run_psi4(
-        geometry=LiH_geometry,
+        geometry=geometry,
         basis=basis,
         multiplicity=1,
         charge=0,
         method="ccsd",
         reference="rhf",
-        freeze_core=False,
+        freeze_core=freeze_core,
         save_hamiltonian=True,
         options=options,
         n_active_extract=None,
@@ -461,13 +278,13 @@ def test_freeze_core_extract_lih_ccsd_amplitudes_parsed_correctly_with_n_active(
     given_fop = parse_amplitudes_from_psi4_ccsd(
         wfn=wfn,
         psi_filename="{0}.dat".format(output_filename),
-        n_active_extract=3,
-        freeze_core_extract=True,
-        freeze_core=False,
-        n_frozen_amplitudes=1,
-        get_mp2_amplitudes=False,
+        n_active_extract=n_active_extract,
+        freeze_core_extract=freeze_core_extract,
+        freeze_core=freeze_core,
+        n_frozen_amplitudes=n_frozen_amplitudes,
+        get_mp2_amplitudes=get_mp2_amplitudes,
     )
 
-    expected_fop = ref_fop
+    expected_fop = expected_fop_fn()
 
     assert expected_fop == given_fop
